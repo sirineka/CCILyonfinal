2f7a77dc1fd3baabb3e46b36aa70ce0f7e77e933
sirineka <karkenisirine@yahoo.fr>
2023-03-20 13:46:01 +0100

    ajouter

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// on récupère l'élèment HTML balise a pour pouvoir ajouter la class open au click
+
+let burgerBtn = document.querySelector('.link-burger');
+
+//on récupère l'élèment Html balise ul avec le menu pour ajouter la classe open au click sur l'élèment burgerBtn
+
+let navbarLinks = document.querySelector('.navbar-links ul');
+
+if (burgerBtn && navbarLinks) {
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.toggle('open');
+        navbarLinks.classList.toggle('open');
+    });
+}
+//function showNavbar(btn, links) {
+   // btn.classList.add('open');
+    //btn.classList.remove('open');
+    // ou
+    //btn.classList.toggle('open');
+    //links.classList.toggle('open');
+
